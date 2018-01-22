214008cd50a111fac42d823854b8fc11bcfacfc3
Dayun Li <dayun@Dayuns-MacBook-Pro.local>
2018-01-21 22:56:56 -0800

    Modify README.

--- email_service/config.py
+++ email_service/config.py
@@ -6,9 +6,9 @@ Config for SendGrid and MailGun email services
 class BaseConfig(object):
     """Base configuration."""
     DEBUG = False
-    SENDGRID_API_KEY = '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'
-    MAILGUN_MESSAGE_BASE_URL = 'https://api.mailgun.net/v3/sannianerban.name/messages'
-    MAILGUN_API_KEY = 'key-edec8464563796ba6741c2dc53ea3144'
+    SENDGRID_API_KEY = # Put your SnedGrid API key here
+    MAILGUN_MESSAGE_BASE_URL = #Put your MailGun base URL here 
+    MAILGUN_API_KEY = # Put your MailGun API key here
 
     # Email Service names
     EMAIL_SERVICES = {
@@ -35,4 +35,3 @@ class TestingConfig(BaseConfig):
 class ProductionConfig(BaseConfig):
     """Production configuration."""
     DEBUG = False
-    DOMAIN_SERVICE = 'email.buyongqucai.com'
