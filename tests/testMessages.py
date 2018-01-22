91e61b2cbbe830379edd036d8d78e1d6d600523f
Dayun Li <dayun@Dayuns-MacBook-Pro.local>
2018-01-21 23:14:43 -0800

    Modify tests to use static string.

--- tests/testMessages.py
+++ tests/testMessages.py
@@ -1,62 +1,63 @@
+from email_service.submit import TO_EMAIL, FROM_EMAIL, SUBJECT, CONTENT
 valid_message = {
-    'to_email': 'lidayun0701@gmail.com',
-    'from_email': 'uber@gmail.com',
-    'subject': 'subject',
-    'content': 'content to send '
+    TO_EMAIL: 'lidayun0701@gmail.com',
+    FROM_EMAIL: 'uber@gmail.com',
+    SUBJECT: SUBJECT,
+    CONTENT: 'content to send '
     }
 
 message_with_empty_to_email = {
-    'to_email': '',
-    'from_email': 'valid@gmail.com',
-    'subject': 'email subject ',
-    'content': 'content'
+    TO_EMAIL: '',
+    FROM_EMAIL: 'valid@gmail.com',
+    SUBJECT: 'email subject ',
+    CONTENT: CONTENT
     }
 
 message_with_invalid_to_email = {
-    'to_email': 'invalid_email',
-    'from_email': 'valid@gmail.com',
-    'subject': 'email subject ',
-    'content': 'content'
+    TO_EMAIL: 'invalid_email',
+    FROM_EMAIL: 'valid@gmail.com',
+    SUBJECT: 'email subject ',
+    CONTENT: CONTENT
     }
 
 message_with_empty_from_email = {
-    'to_email': 'lidayun0701@gmail.com',
-    'from_email': '',
-    'subject': 'email subject ',
-    'content': 'content'
+    TO_EMAIL: 'lidayun0701@gmail.com',
+    FROM_EMAIL: '',
+    SUBJECT: 'email subject ',
+    CONTENT: CONTENT
     }
 
 message_with_invalid_from_email = {
-    'to_email': 'lidayun0701@gmail.comm',
-    'from_email': 'invalid_from_email',
-    'subject': 'email subject ',
-    'content': 'content'
+    TO_EMAIL: 'lidayun0701@gmail.comm',
+    FROM_EMAIL: 'invalid_from_email',
+    SUBJECT: 'email subject ',
+    CONTENT: CONTENT
     }
 
 message_with_empty_content = {
-    'to_email': 'lidayun0701@gmail.com',
-    'from_email': 'valid@gmail.com',
-    'subject': 'email subject',
-    'content' : ''
+    TO_EMAIL: 'lidayun0701@gmail.com',
+    FROM_EMAIL: 'valid@gmail.com',
+    SUBJECT: 'email subject',
+    CONTENT : ''
     }
 
 message_with_empty_too_long_content = {
-    'to_email': 'lidayun0701@gmail.com',
-    'from_email': 'valid@gmail.com',
-    'subject': 'subject',
-    'content': 10001 * '.'
+    TO_EMAIL: 'lidayun0701@gmail.com',
+    FROM_EMAIL: 'valid@gmail.com',
+    SUBJECT: SUBJECT,
+    CONTENT: 10001 * '.'
     }
 
 message_with_empty_subject = {
-    'to_email': 'lidayun0701@gmail.com',
-    'from_email': 'valid@gmail.com',
-    'subject': '',
-    'content': 'content with no subject to send '
+    TO_EMAIL: 'lidayun0701@gmail.com',
+    FROM_EMAIL: 'valid@gmail.com',
+    SUBJECT: '',
+    CONTENT: 'content with no subject to send '
     }
 
 message_with_empty_too_long_subject = {
-    'to_email': 'lidayun0701@gmail.com',
-    'from_email': 'valid@gmail.com',
-    'subject': 1001 * '.',
-    'content': 'content to send '
+    TO_EMAIL: 'lidayun0701@gmail.com',
+    FROM_EMAIL: 'valid@gmail.com',
+    SUBJECT: 1001 * '.',
+    CONTENT: 'content to send '
     }
